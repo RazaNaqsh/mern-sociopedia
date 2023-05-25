f37606289ff1012c417783b510f18fefd4f13b15
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-25 19:54:17 +0530

    Create postModels and createPost controller

---
+++ server/controllers/posts.js
@@ -0,0 +1,31 @@
+import Post from "../models/Posts.js";
+import User from "../models/User.js";
+
+/* CREATE */
+
+export const createPosts = async (req, res) => {
+	try {
+		const { userId, description, picturePath } = req.body;
+		const user = await User.findById(userId);
+
+		const newPost = new Post({
+			userId,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			location: user.location,
+			description,
+			userPicturePath: user.picturePath,
+			picturePath,
+			likes: {},
+			comments: [],
+		});
+
+		await newPost.save();
+
+		const post = await Post.find();
+
+		res.status(201).json(post);
+	} catch (err) {
+		res.status(409).json({ message: err.message });
+	}
+};
