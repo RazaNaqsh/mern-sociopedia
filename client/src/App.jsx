864338ad82c750c4cbef4e18eb451a69ca589295
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-30 22:03:15 +0530

    Setting up app and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={<LoginPage />}
+					/>
+					<Route
+						path="/home"
+						element={<HomePage />}
+					/>
+					<Route
+						path="/profile/:userId"
+						element={<ProfilePage />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
