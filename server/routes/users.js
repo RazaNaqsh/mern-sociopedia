eaf1f16f18f722e3eff4fe789f2c96f7de9beeb2
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-03 19:50:29 +0530

    Codebase changed

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,9 @@
 import express from "express";
 import {
-	getUser,
-	getUserFriends,
-	addRemoveFriend,
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
 } from "../controllers/users.js";
-
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
